06c260a90ff348238c52b0f80f32af38a9f15ac8
unknown <artyom@Artyom-PC.solidsystems.local>
2016-11-18 14:55:43 +0300

    fix problem in DateBuilder.class

--- common/src/utils/DateBuilder.java
+++ common/src/utils/DateBuilder.java
@@ -8,17 +8,12 @@ public class DateBuilder {
     private static Calendar calendar = Calendar.getInstance();
 
     public static Date endDay(Date date) {
-
         calendar.setTime(date);
         calendar.add(Calendar.DATE, 1);
-        calendar.set(Calendar.HOUR, 0);
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.setTime(getDateWithoutTime(calendar.getTime()));
         calendar.add(Calendar.MILLISECOND, -1);
         return calendar.getTime();
-    }// TODO: Kirill это метод "конец дня"..
+    }// TODO: Kirill это метод "конец дня".. ::: отрефакторил
     // а это "дата без времени"...
     public static Date getDateWithoutTime(Date date) {
         calendar.setTime(date);
@@ -31,15 +26,17 @@ public class DateBuilder {
 
     public static DateFilter getLastMonth() {        
         // TODO: 16.10.2016 вообще создаешь в методе календарь один и давай сним работать. ::: исправил
-        // TODO: Kirill я плакаю, подумай хорошо и приходи расскажи, что ты сделал.
+        // TODO: Kirill я плакаю, подумай хорошо и приходи расскажи, что ты сделал.  ::: не задано было время откуда высчитывать прошлый месяц
         DateFilter dateFilter = new DateFilter();
+        Date today = new Date();
+        calendar.setTime(today);
         calendar.add(Calendar.MONTH, - 1);
         calendar.set(Calendar.HOUR, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
         dateFilter.setFrom(calendar.getTime());
-        calendar = Calendar.getInstance(); // TODO: 16.10.2016 уже с датой ::: убрал setTime(new Date())
+        calendar.setTime(today); // TODO: 16.10.2016 уже с датой ::: убрал setTime(new Date())
         calendar.add(Calendar.DATE, -1);
         dateFilter.setTo(calendar.getTime());
         return dateFilter;
